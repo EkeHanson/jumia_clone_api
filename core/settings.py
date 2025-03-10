81b56b55579c7f2fd0eb4a5d2d2195efa9b8ea43
YourGitHubUsername <EkeHanson>
2025-03-10 21:31:59 +0100

    database now on render

--- core/settings.py
+++ core/settings.py
@@ -110,6 +110,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'jumia_clone_db',
+#         'USER': 'jumia_clone_db_user',
+#         'PASSWORD': 'm3B2qKV5O4wPxnrlODVWLulYeGEKMmFA',
+#         'HOST': 'dpg-cv7kjii3esus73ehiah0-a.oregon-postgres.render.com',
+#         'PORT': '5432',
+#     }
+# }
+
 
 DATABASES = {
     'default': {
@@ -123,12 +134,12 @@ DATABASES = {
 }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
